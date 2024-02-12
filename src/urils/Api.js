61f2a8b74b8f507f2817b3aa2c0d3e9e2294cdcd
ZaoBaoo <yurkin.daniil96@gmail.com>
2024-02-12 23:33:13 +0600

    Добавил события для квестов и Api

---
+++ src/urils/Api.js
@@ -0,0 +1,54 @@
+class Api {
+  pushEventCompletedGame = (balance, idQuest) => {
+    if (window.dataLayer) {
+      window.dataLayer.push({
+        event: "bf_21_quest_complete",
+        quest_name: idQuest,
+        barsik_balance: balance,
+      });
+    } else {
+      // eslint-disable-next-line
+      r46("track", "quest_complete", {
+        category: `0`,
+        label: idQuest,
+      });
+      // eslint-disable-next-line
+      r46("profile", "set", {
+        [idQuest]: "complete",
+      });
+    }
+  };
+
+  // getProfileUserFetch = async () => {
+  //     const did = localStorage.getItem('rees46_device_id');
+  //
+  //     return new Promise((resolve, reject) => {
+  //         try {
+  //             fetch(`https://api.r46.technodom.kz/profile?did=${'did'}&shop_id=74fd3b613553b97107bc4502752749`).then(res => res.json()).then(data => resolve(data))
+  //         } catch (e) {
+  //             reject(new Error(`Что-то пошло не так при получении профиля пользователя. Ваш did: ${did}`))
+  //         }
+  //     })
+  // }
+
+  getProfileUser = async () => {
+    return new Promise((resolve, reject) => {
+      try {
+        // eslint-disable-next-line
+        r46("profile", "get", function (profile) {
+          if (profile) {
+            resolve(profile);
+          } else {
+            reject(new Error("Не удалось получить профиль пользователя"));
+          }
+        });
+      } catch (e) {
+        reject(
+          new Error("Что-то пошло не так при получении профиля пользователя"),
+        );
+      }
+    });
+  };
+}
+
+export const api = new Api();
--- src/urils/brandsData.js
+++ src/urils/brandsData.js
@@ -2,7 +2,17 @@ export const brandsData = {
   ava: {
     startDate: "February 12, 2024 10:00:00",
     slug: "ava",
-    quests: {},
+    quests: [
+      {
+        id: "cn_ny_2024_ava_quest_1",
+        endContent: {
+          title: "Ты всё ближе к цели! 1-ый уровень позади!",
+          text: "Ты участвуешь в розыгрыше 50 сертификатов номиналом 10 000 тенге",
+        },
+        isCompleted: false,
+        image: "https://www.technodom.kz/under/lunar-new-year/ava-prize-1.png",
+      },
+    ],
   },
   neo: {
     startDate: "February 14, 2024 10:00:00",
