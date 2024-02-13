4c966858bcf04dc943e83ac60dc169a29efe92d1
ZaoBaoo <yurkin.daniil96@gmail.com>
2024-02-14 00:30:22 +0600

    Сделал корректное переключение квестов

--- src/components/SceneSwitcher/components/QuestСomplete/QuestСomplete.jsx
+++ src/components/SceneSwitcher/components/QuestСomplete/QuestСomplete.jsx
@@ -2,46 +2,46 @@ import "./QuestСomplete.scss";
 import { Brand } from "../../../Brand/Brand.jsx";
 import { Button } from "../../../Button/Button.jsx";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../../../urils/Api.js";
-import {
-  handleCompleteAndInstallNextQuestAction,
-  setActiveQuestAction,
-  setActiveSceneAction,
-  setBrandsAction,
-} from "../../../../store/reducers/scene.js";
+import { handleCompleteAndInstallNextQuestAction } from "../../../../store/reducers/scene.js";
+import { Loader } from "../../../Loader/Loader.jsx";
 
 const QuestComplete = () => {
+  const [currentQuest, setCurrentQuest] = useState(null);
   const { brands, activeQuest, activeBrand } = useSelector(
-    (state) => state.scene
+    (state) => state.scene,
   );
 
   const dispatch = useDispatch();
 
   const nextQuest = () => {
     setTimeout(() => {
-      dispatch(dispatch(handleCompleteAndInstallNextQuestAction()));
+      handleCompleteAndInstallNextQuest();
     }, 500);
   };
 
-  // const handleCompleteAndInstallNextQuestAction = () => {
-  //   const { event } = activeQuest;
-  //
-  //   const targetBrand = JSON.parse(JSON.stringify(brands))[activeBrand];
-  //
-  //   let indexNextQuest;
-  //
-  //   targetBrand.quests.forEach((quest, index) => {
-  //     if (quest.event === event) {
-  //       quest.complete = true;
-  //       indexNextQuest = index + 1;
-  //     }
-  //   });
-  //
-  //   dispatch(setBrandsAction({ ...brands, [activeBrand]: targetBrand }));
-  //   dispatch(setActiveQuestAction(targetBrand.quests[indexNextQuest]));
-  //   dispatch(setActiveSceneAction({}));
-  // };
+  const handleCompleteAndInstallNextQuest = () => {
+    const copyObjectBrands = JSON.parse(JSON.stringify(brands));
+
+    const indexNextQuest = brands[activeBrand].quests.findIndex(
+      (brand) => brand.event === activeQuest.event,
+    );
+
+    copyObjectBrands[activeBrand].quests[indexNextQuest].complete = true;
+    const nextQuest = JSON.parse(
+      JSON.stringify(copyObjectBrands[activeBrand].quests[indexNextQuest + 1]),
+    );
+    const nextScene = nextQuest.nameQuest;
+
+    dispatch(
+      handleCompleteAndInstallNextQuestAction({
+        copyObjectBrands,
+        nextQuest,
+        nextScene,
+      }),
+    );
+  };
 
   useEffect(() => {
     if (activeQuest) {
@@ -51,27 +51,43 @@ const QuestComplete = () => {
     }
   }, [activeQuest]);
 
+  useEffect(() => {
+    if (activeQuest) {
+      setCurrentQuest(activeQuest);
+    }
+  }, []);
+
   return (
     <div className="questComplete">
-      <div className="questComplete__wrapper">
-        <div className="questComplete__top">
-          <Brand size="large" type="ava" />
-          <h3 className="questComplete__title">
-            Ты всё ближе к цели! 1-ый уровень позади!
-          </h3>
-          <p className="questComplete__text">
-            Ты участвуешь в розыгрыше 50 сертификатов номиналом 10 000 тенге
-          </p>
-        </div>
-        <div className="questComplete__bottom">
-          <Button size="large" onClick={nextQuest} text="К следующему уровню" />
-        </div>
-      </div>
-      <img
-        className="questComplete__image"
-        src="https://www.technodom.kz/under/lunar-new-year/ava-prize-1.png"
-        alt=""
-      />
+      {currentQuest ? (
+        <>
+          <div className="questComplete__wrapper">
+            <div className="questComplete__top">
+              <Brand size="large" type={activeBrand} />
+              <h3 className="questComplete__title">
+                {currentQuest.endContent.title}
+              </h3>
+              <p className="questComplete__text">
+                {currentQuest.endContent.text}
+              </p>
+            </div>
+            <div className="questComplete__bottom">
+              <Button
+                size="large"
+                onClick={nextQuest}
+                text="К следующему уровню"
+              />
+            </div>
+          </div>
+          <img
+            className="questComplete__image"
+            src={currentQuest.image}
+            alt=""
+          />
+        </>
+      ) : (
+        <Loader />
+      )}
     </div>
   );
 };
--- src/store/reducers/scene.js
+++ src/store/reducers/scene.js
@@ -21,34 +21,12 @@ export const slice = createSlice({
     setActiveQuestAction: (state, { payload }) => {
       state.activeQuest = payload;
     },
-    handleCompleteAndInstallNextQuestAction: (state) => {
-      const { brands, activeBrand, activeQuest } = state;
-      const { event } = activeQuest;
-      const currentBrand = brands[activeBrand];
+    handleCompleteAndInstallNextQuestAction: (state, { payload }) => {
+      const { copyObjectBrands, nextQuest, nextScene } = payload;
 
-      const indexNextQuest = currentBrand.quests.findIndex(
-        (brand) => brand.event === event
-      );
-
-      console.log(
-        JSON.parse(
-          JSON.stringify(state.brands[activeBrand].quests[indexNextQuest])
-        )
-      );
-
-      // currentBrand.quests.forEach((brand, index) => {
-      //   if (brand.event === event) {
-      //     brand.complete = true;
-      //
-      //     indexNextQuest = index + 1;
-      //     // state.activeQuest = nextQuest;
-      //     // state.activeScene = nextQuest.nameQuest;
-      //   }
-      // });
-
-      // const nextQuest = currentBrand.quests[indexNextQuest];
-      // state.activeQuest = nextQuest;
-      // state.activeScene = nextQuest.nameQuest;
+      state.brands = copyObjectBrands;
+      state.activeQuest = nextQuest;
+      state.activeScene = nextScene;
     },
   },
 });
