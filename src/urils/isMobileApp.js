1c623d55dfd923bc08ed6f470eaadec01ca2134b
Yurkin Daniil <zaobaoo@yandex.kz>
2024-02-23 10:34:31 +0600

    Пофиксил функцию isMobileApp

--- src/urils/isMobileApp.js
+++ src/urils/isMobileApp.js
@@ -2,9 +2,5 @@ export const isMobileApp = () => {
   const queryParams = new URLSearchParams(document.location.search);
   const isMobileApp = queryParams.get("source");
 
-  if (window.innerWidth < 700 && isMobileApp === "mobile-app") {
-    return true;
-  } else {
-    return false;
-  }
+  return window.innerWidth < 700 && isMobileApp === "mobile-app";
 };
