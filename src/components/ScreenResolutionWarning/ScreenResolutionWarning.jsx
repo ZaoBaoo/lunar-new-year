4d7dacfdf1f57cf632d808163470ec79f0aaf29c
Daniil Yurkin <fday@technodom.kz>
2025-06-04 14:05:08 +0500

    feat: removed app requirement

--- src/components/ScreenResolutionWarning/ScreenResolutionWarning.jsx
+++ src/components/ScreenResolutionWarning/ScreenResolutionWarning.jsx
@@ -8,23 +8,23 @@ const ScreenResolutionWarning = () => {
 
   return (
     <div className="screenResolutionWarning">
-      <p>Игра доступна только в мобильном приложении Technodom</p>
-      <a
-        className="screenResolutionWarning__link"
-        href="https://technodomkz.page.link/ny24"
-      >
-        <img
-          src="https://www.technodom.kz/under/lunar-new-year/link-app.png"
-          alt=""
-        />
-      </a>
+      <p>Игра доступна только на телефоне</p>
+      {/*<a*/}
+      {/*  className="screenResolutionWarning__link"*/}
+      {/*  href="https://technodomkz.page.link/ny24"*/}
+      {/*>*/}
+      {/*  <img*/}
+      {/*    src="https://www.technodom.kz/under/lunar-new-year/link-app.png"*/}
+      {/*    alt=""*/}
+      {/*  />*/}
+      {/*</a>*/}
 
-      <Button
-        className="screenResolutionWarning__button"
-        text="Скачать"
-        size="large"
-        onClick={handleSetLocation}
-      />
+      {/*<Button*/}
+      {/*  className="screenResolutionWarning__button"*/}
+      {/*  text="Скачать"*/}
+      {/*  size="large"*/}
+      {/*  onClick={handleSetLocation}*/}
+      {/*/>*/}
     </div>
   );
 };
--- src/urils/isMobileApp.js
+++ src/urils/isMobileApp.js
@@ -2,5 +2,6 @@ export const isMobileApp = () => {
   const queryParams = new URLSearchParams(document.location.search);
   const isMobileApp = queryParams.get("source");
 
-  return window.innerWidth < 700 && isMobileApp === "mobile-app";
+  // return window.innerWidth < 700 && isMobileApp === "mobile-app";
+  return window.innerWidth < 700;
 };
